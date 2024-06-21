f047842314adfbb9bae78e685d0ededfdb902de2
TARUN TEJASHWI SHARMA <developer.rytclick@gmail.com>
2024-06-21 19:30:11 +0530

    let c

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use('/api',ApiRoutes)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 
 app.get('/',(req,res)=>{
   res.send('Hello server')
