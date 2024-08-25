a52079f5d99bee4c4d9d01457179d3773d641c9e
TARUN TEJASHWI SHARMA <torque061@gmail.com>
2024-08-25 14:24:56 +0530

    Update htmlExtractor.js

--- utils/htmlExtractor.js
+++ utils/htmlExtractor.js
@@ -9,7 +9,7 @@ const extractContent = (htmlContent) => {
     for (let i = 1; i <= 6; i++) {
         // Correct the selector to 'h' + i instead of 'h1' + i
         const headingElements = document.querySelectorAll(`h${i}`);
-        headings[h`${i}`] = Array.from(headingElements).map(heading => heading.textContent.trim());
+        headings[`h${i}`] = Array.from(headingElements).map(heading => heading.textContent.trim());
     }
 
     const paragraphs = Array.from(document.querySelectorAll('p')).map(p => p.textContent.trim());
