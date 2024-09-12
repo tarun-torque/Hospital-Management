164a61441ccb384c9ba7bdd8174289947f49ad8d
TARUN TEJASHWI SHARMA <torque061@gmail.com>
2024-09-12 12:20:26 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ const router = Router()
 
 
 import {create_yt_Content, create_blog_content, create_arcticle_content, get_all_content, get_profile, update_article, update_yt, update_blog, delete_yt, delete_article, delete_blog, search_creator, login_creator, stateContent, languagePost, categoryContent, get_blogs, eachBlog, eachArticle, eachYT, eachCreator } from "../controllers/creater.controller.js";
-import { addDoctorService, adminSearchBar, allArticle, allBlog, allDoctors, allYt, bookSlot, CreateDoctor_profile, creatorSearchBar, deleteDoctor_profile, deletePatientSupport, doctorLogin, eachSupport, getAllAvailableSlots, getAllRecentTicket, getAvailableSlotsDoctor, getDoctorProfile, getDoctorsByServiceId, getServiceFromId, getServicesByDoctorId, managerSearchBar, patientAllSupport, patientSupport, recentTicket, searchDoctorAndServices, signInDoctorFromGoogle, trendingConsultant, upcomingSession, updateAvailability, updateDoctorProfile, updateDoctorRemarks, updateDoctorStatus, updateSupport } from "../controllers/doctor.controller.js";
+import { addDoctorService, adminSearchBar, allArticle, allBlog, allDoctors, allYt, bookSlot, CreateDoctor_profile, creatorSearchBar, deleteDoctor_profile, deletePatientSupport, doctorLogin, doctorPrice, eachSupport, getAllAvailableSlots, getAllRecentTicket, getAvailableSlotsDoctor, getDoctorPrice, getDoctorProfile, getDoctorsByServiceId, getServiceFromId, getServicesByDoctorId, managerSearchBar, patientAllSupport, patientSupport, recentTicket, searchDoctorAndServices, signInDoctorFromGoogle, trendingConsultant, upcomingSession, updateAvailability, updateDoctorPrice, updateDoctorProfile, updateDoctorRemarks, updateDoctorStatus, updateSupport } from "../controllers/doctor.controller.js";
 import { delete_support, get_mood, get_support, getGooglePatientProfile, loginPatient, mood, otpSend, post_support, registerPatient, resetPassword, signInPatientFromGoogle, test, update_support, verifyPatientEmail, verifyPatientOTP } from "../controllers/patient.controller.js";
 import { creator_profile,approveDoctorRequest, contentCategory, deleteCategory, getActiveDoctors, getApprovedDoctors, getInactiveDoctors, getPendingDoctors, getRejectedDoctors, getTemporaryoffDoctors, register_manager, rejectDoctor, getContentCategory, update_ContentCategory, getAllManager, delete_manager, updateManager, filterPatient, allPatient, getCreators, setInactiveManager, setOffManager, getActiveManager, getInactiveManager, getOffManager, setActiveManager, updateRemarks, deleteCreator, setInactiveCreator, setActiveCreator, setOffCreator, activeCreators, inactiveCreators, offCreators, updateRemarkCreator, assignManager_doctor, updateCreatorProfile,statusOfContent , articleAction, blogAction, ytAction, staff, allContentAdmin, category, updateCategory, allCategory, categoryDelete, createService, updateService, deleteService, allService, getServiceFromCategoryId, getServiceFromServiceId, topArticle, topBlogs, topYt, consultants, registeredUser, adminLogin} from "../controllers/admin.controller.js";
 import {eachManager, getContentByManager, login_manager } from "../controllers/manager.controller.js";
@@ -131,6 +131,33 @@ router.get('/get/all/recent/ticket',getAllRecentTicket)
 
 router.post('/doctor/google/signIn',signInDoctorFromGoogle)
 router.post('/doctor/add/service',addDoctorService)
+
+
+// -------------------doctor add price 
+router.post('/doctor/price/:doctorId/:serviceId',doctorPrice)
+router.put('/doctor/update/price/:doctorId/:serviceId',updateDoctorPrice)
+router.get('/get/doctor/price/:doctorId/:serviceId',getDoctorPrice)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 router.post('/createDoctorProfile', upload.fields([{ name: 'doctorProfile', maxCount: 1 }, { name: 'doctorDocument', maxCount: 1 }]), CreateDoctor_profile)
 router.post('/doctor/login',doctorLogin)
