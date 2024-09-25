bb401808309542b0fbe35723e5769ba0ef3e1bbc
TARUN TEJASHWI SHARMA <torque061@gmail.com>
2024-09-25 11:07:48 +0530

    Update doctor.controller.js

--- controllers/doctor.controller.js
+++ controllers/doctor.controller.js
@@ -876,21 +876,21 @@ export const signInDoctorFromGoogle = async (req, res) => {
         }
         let doctor = await prisma.doctor.findUnique({ where: { email } });
 
-        const data = { username, email, profileUrl, fcmToken };
-        const token = jwt.sign({ data }, process.env.SECRET_KEY, { expiresIn: '999h' })
+        const data = { username, email, profileUrl, fcmToken }
+        // const token = jwt.sign({ data }, process.env.SECRET_KEY, { expiresIn: '999h' })
 
         if (doctor) {
-
             const updateDocotor = await prisma.doctor.update({
                 where: { email },
                 data: { fcmToken }
             })
+            const token = jwt.sign({ updateDocotor }, process.env.SECRET_KEY, { expiresIn: '999h' })
             return res.status(200).json({ status: 200, msg: 'Token refreshed', token,id:updateDocotor.id });
         } else {
-            const saveDoctor = await prisma.doctor.create({ data });
-            return res.status(201).json({ status: 201, msg: 'Profile created successfully', token, id: saveDoctor.id });
+            const saveDoctor = await prisma.doctor.create({ data })
+            const newToken = jwt.sign({ saveDoctor }, process.env.SECRET_KEY, { expiresIn: '999h' })
+            return res.status(201).json({ status: 201, msg: 'Profile created successfully', token:newToken, id: saveDoctor.id });
         }
-        
     } catch (error) {
         res.status(500).json({ status: 500, msg: error.message });
     }
