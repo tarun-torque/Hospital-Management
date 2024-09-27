ca66010ef18326d5fc43559a3552b511f6e18028
TARUN TEJASHWI SHARMA <torque061@gmail.com>
2024-09-27 12:55:33 +0530

    Update doctor.controller.js

--- controllers/doctor.controller.js
+++ controllers/doctor.controller.js
@@ -1996,11 +1996,6 @@ export const verifyPatientOtp = async (req, res) => {
 //     }
 // }
 
-import moment from 'moment-timezone';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
 export const getOneHourSlots = async (req, res) => {
     const doctorId = +req.params.doctorId;
 
